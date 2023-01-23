ac73b54a70b2c3316d89094a9fd01ebec71424ea
IslamAltayeb <91779854+IslamAltayeb@users.noreply.github.com>
2023-01-23 22:17:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,29 @@
 const myForm = document.querySelector("#my-form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
-const msg = document.querySelector("#msg");
+const msg = document.querySelector(".msg");
 const userList = document.querySelector("#users");
+const theAge = document.querySelector('#age');
                
 myForm.addEventListener('submit',onsubmit);
 
-function submit(e) {
+myForm.addEventListener('submit', (e)=>{
     e.preventDefault();
-
-    if (nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error');
-        msg.innerHTML = 'please enter all fields';
-        setTimeout(() => msg.remove(),3000);
-    }else{
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-        userList.appendChild(li);
-
-        //clear fields
-        nameInput.value = '';
-        emailInput.value = '';
+    if (nameInput.value === ''|| emailInput.value ==='' )
+    {
+      msg.classList.add('error');
+      msg.innerHTML = 'Please enter all fields';
+      //time out to delete error message after 3s     
+      setTimeout(() => {msg.remove();}, 3000);
+      
     }
-}
+    else{
+     const li = document.createElement('li');
+     li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+     userList.appendChild(li);
+  
+     //clear fields
+     nameInput.value ='';
+     emailInput.value = '';
+    }
+  });
